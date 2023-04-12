74260305cee76c02bab5b0d94b7cbe87199fb5dc
SebObando <sebastian.obando@inspirato.io>
2023-04-12 17:11:45 -0500

    cleaning

--- data_platform_deployment.py
+++ data_platform_deployment.py
@@ -6,7 +6,6 @@ def set_index_defintion_changed_variable(file_name: str) -> None:
     is_current_branch_main = git_client.is_current_branch_main()
 
     if is_current_branch_main:
-        command = "git diff HEAD^"
         output = git_client.get_diff_between_last_two_merges()
     else:
         command = "git diff --name-only origin/main"
